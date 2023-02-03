08a46a2cf46067b7eaaff8d76b0617e1086d9062
Anastasiia Shlapak <anastasiashlapak@icloud.com>
2023-02-03 20:56:14 +0100

    finish task

---
+++ src/components/Counter/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import styles from "./Counter.module.css";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      isAdd: true,
+      click: false,
+    };
+  }
+  changeHandler = () => {
+    this.setState({
+      isAdd: !this.state.isAdd,
+    });
+  };
+  clickHandler = () => {
+    if (this.state.isAdd) {
+      this.setState({
+        count: this.state.count + Number(this.props.step),
+      });
+    } else {
+      this.setState({
+        count: this.state.count - Number(this.props.step),
+      });
+    }
+  };
+  render() {
+    const { count, isAdd } = this.state;
+    const buttonValue = isAdd ? "Add" : "Sub";
+    return (
+      <article>
+        <h1 className={styles.header}>Counter</h1>
+        <h2>{count}</h2>
+        <div>
+          <button className={styles.button} onClick={this.clickHandler}>
+            {buttonValue}
+          </button>
+          <button className={styles.button} onClick={this.changeHandler}>
+            Change Mode
+          </button>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default Counter;
---
+++ src/components/CounterSection/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Counter from './../Counter';
+import styles from './CounterSection.module.css'
+
+class CounterSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: "",
+    };
+  }
+//   stepHandler = (event) => {
+//     this.setState({
+//       step: event.target.value,
+//     });
+//   };
+
+  stepHandler = (event) => {
+    if (event.target.value > 0 && event.target.value < 1000000) {
+      this.setState({
+        step: event.target.value,
+      });
+    } 
+  };
+
+  render() {
+    return (
+      <div className={styles.wrapper}>
+        <Counter step={this.state.step} />
+        <h2>Step: {this.state.step}</h2>
+        <input 
+          onChange={this.stepHandler}
+          className={styles.input}
+          value={this.state.step}
+          type="number"
+          name="step"
+          placeholder="enter your step"
+        ></input>
+        
+      </div>
+    );
+  }
+}
+
+export default CounterSection;
